240f1245fd55335b31b82b6b63e97491c7e61ced
abdelali <abdelalietu@gmail.com>
2019-12-01 16:00:00 +0100

    ajout question1&question2

--- question2.php
+++ question2.php
@@ -1,5 +1,43 @@
 <?php
- include_once "classes.php";
+include_once "classes.php";
+$result = "";
+if(isset($_POST['point1']) && !empty($_POST['point1']) &&
+    isset($_POST['point2']) && !empty($_POST['point2']) &&
+    isset($_POST['point3']) && !empty($_POST['point3'])) {
+    $point1 = $_POST['point1'];
+    $point2 = $_POST['point2'];
+    $point3 = $_POST['point3'];
+    $point1 = explode(',',$point1);
+    $point2 = explode(',',$point2);
+    $point3 = explode(',',$point3);
+
+    $x1 = $point1[0];
+    $y1 = $point1[1];
+
+    $x2 = $point2[0];
+    $y2 = $point2[1];
+
+    $x3 = $point3[0];
+    $y3 = $point3[1];
+
+    $pointClass1 = new Point($x1,$x1);
+    $pointClass2 = new Point($x2,$x2);
+    $pointClass3 = new Point($x3,$x3);
+    $triangle = new Triangle($pointClass1, $pointClass2, $pointClass3);
+    if($_POST['question'] === "isocele") {
+        $result = $triangle->isocele() ? "True: Donc le triangle est isocèle" : "False: Le triangle n'est pas isocèle";
+    }
+    if($_POST['question'] === "equilateral") {
+        $result = $triangle->equilateral() ? "True: Le triangle est equilateral" : "False: Le triangle n'est pas equilateral";
+    }
+    if($_POST['question'] === "rectangle") {
+        $result = $triangle->rectangle() ? "True: Le triangle est rectangle" : "False: Le triangle n'est pas rectangle";
+    }
+} else {
+    if(isset($_POST['question'])) {
+        $result = "les champs sont obligatoire";
+    }
+}
 ?>
 <!doctype html>
 <html lang="en">
@@ -18,7 +56,7 @@
             </a>
         </div>
         <div class="col-sm-4">
-            <a href="question2.php" class="menu-item active d-block">
+            <a href="question2.php" class="menu-item active-question d-block">
                 Question 2
             </a>
         </div>
@@ -30,24 +68,83 @@
         <div class="col-sm-12">
             <div class="form">
                 <div class="row">
+                    <?php if($result) { ?>
+                    <div class="col-md-12">
+                        <div class="alert alert-success"><?php echo $result ?></div>
+                    </div>
+                    <?php  }  ?>
                     <div class="col-md-6">
-                        Un triangle est constitué par 3 points. Définir une classe Triangle avec trois propriétés
-                        pt1, pt2 et pt3 (les coins d’un triangle), et les méthodes suivantes :<br>
-                        — constructeur qui sert à définir les propriétés de la classe<br>
-                        — isocele qui retourne True si le triangle est isocèle ou False si non<br>
-                        — equilateral qui retourne True si le triangle est équilatéral ou False si non<br>
-                        — rectangle qui retourne True si le triangle est rectangle ou False si non<br><br>
-                        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
-                            <div class="form-group">
-                                <input type="text" class="form-control-lg" placeholder="Entrez le point X1,Y1">
+                        <div class="nav nav-pills" id="v-pills-tab" role="tablist">
+                            Un triangle est constitué par 3 points. Définir une classe Triangle avec trois propriétés
+                            pt1, pt2 et pt3 (les coins d’un triangle), et les méthodes suivantes :<br>
+                            <!--                            <a class="active" id="v-pills-one-tab" data-toggle="pill" href="#v-pills-one" role="tab" aria-controls="v-pills-one" aria-selected="true">-->
+                            — constructeur qui sert à définir les propriétés de la classe<br>
+                            <!--                            </a>-->
+                            <a id="v-pills-two-tab" data-toggle="pill" href="#v-pills-two" role="tab" aria-controls="v-pills-two" aria-selected="false">
+                                — isocele qui retourne True si le triangle est isocèle ou False si non<br>
+                            </a>
+                            <a id="v-pills-three-tab" data-toggle="pill" href="#v-pills-three" role="tab" aria-controls="v-pills-three" aria-selected="false">
+                                — equilateral qui retourne True si le triangle est équilatéral ou False si non<br>
+                            </a>
+                            <a id="v-pills-four-tab" data-toggle="pill" href="#v-pills-four" role="tab" aria-controls="v-pills-four" aria-selected="false">
+                                — rectangle qui retourne True si le triangle est rectangle ou False si non<br><br>
+                            </a>
+                        </div>
+                        <div class="tab-content" id="v-pills-tabContent">
+                            <!--                            <div class="tab-pane fade" id="v-pills-one" role="tabpanel" aria-labelledby="v-pills-one-tab">-->
+                            <!--                            </div>-->
+                            <div class="tab-pane fade show active" id="v-pills-two" role="tabpanel" aria-labelledby="v-pills-two-tab">
+                                <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+                                    <input type="hidden" name="question" value="isocele">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control-lg" name="point1" placeholder="Entrez le point X1,Y1">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control-lg" name="point2" placeholder="Entrez le point X2,Y2">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control-lg" name="point3" placeholder="Entrez le point X3,Y3">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="submit" class="btn btn-primary calc btn-lg" value="Tester si le triangle est isocele">
+                                    </div>
+                                </form>
                             </div>
-                            <div class="form-group">
-                                <input type="text" class="form-control-lg" placeholder="Entrez le point X2,Y2">
+                            <div class="tab-pane fade" id="v-pills-three" role="tabpanel" aria-labelledby="v-pills-three-tab">
+                                <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+                                    <input type="hidden" name="question" value="equilateral">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control-lg" name="point1" placeholder="Entrez le point X1,Y1">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control-lg" name="point2" placeholder="Entrez le point X2,Y2">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control-lg" name="point3" placeholder="Entrez le point X3,Y3">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="submit" class="btn btn-primary calc btn-lg" value="Tester si le triangle est equilateral">
+                                    </div>
+                                </form>
                             </div>
-                            <div class="form-group">
-                                <input type="text" class="form-control-lg" placeholder="Entrez le point X3,Y3">
+                            <div class="tab-pane fade" id="v-pills-four" role="tabpanel" aria-labelledby="v-pills-four-tab">
+                                <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+                                    <input type="hidden" name="question" value="rectangle">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control-lg" name="point1" placeholder="Entrez le point X1,Y1">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control-lg" name="point2" placeholder="Entrez le point X2,Y2">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control-lg" name="point3" placeholder="Entrez le point X3,Y3">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="submit" class="btn btn-primary calc btn-lg" value="Tester si le triangle est rectangle">
+                                    </div>
+                                </form>
                             </div>
-                        </form>
+                        </div>
                     </div>
                     <div class="col-md-6 bg-white">
                         <div class="img-container">
